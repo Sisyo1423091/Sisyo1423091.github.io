e8868addb1cd46e7b4c4e164cd5ffd7564329058
Sisyo1423091 <35915714+Sisyo1423091@users.noreply.github.com>
2018-01-30 00:40:55 +0900

    update

--- main.js
+++ main.js
@@ -1,40 +1,3 @@
-//基盤IDの設定
-const UUID = '19b10000-e8f2-537e-4f6c-d104768a1214';
-const SERVICE_UUID = '19b10001-e8f2-537e-4f6c-d104768a1214';
-
-//受信データの格納
-let g_characteristic = {};
-
-var diceValue = 6;
-
-//接続処理
-const main = () => {
-    return navigator.bluetooth.requestDevice({acceptAllDevices:true, optionalServices:[UUID]})
-    .then(device => {
-      console.log(device);
-      console.log('> Name:             ' + device.name);
-      console.log('> Id:               ' + device.id);
-      console.log('> Connected:        ' + device.gatt.connected);
-      console.log('--デバイスに接続中です...--');
-      return device.gatt.connect();
-    })
-    .then(server => {
-        console.log('Getting ___ Service...');
-        return server.getPrimaryService(UUID);
-    })
-    .then(service => {
-        console.log('Getting ____ Characteristic...');
-        return service.getCharacteristic(SERVICE_UUID);
-      })
-    .then(characteristic => {
-        g_characteristic = characteristic;
-        return characteristic;
-      })
-    .catch(error => {
-      console.log('Argh! ' + error);
-    });
-}
-
 window.onload = function()
 {
     instantiateButton();
@@ -68,16 +31,4 @@ function setDiceValue()
         return;
     }
     alert("数値を入力してください。");
-}
-
-//値の受信
-const valueReception = () => {
-    g_characteristic.readValue()
-    .then((val) => {
-        let v = val.getUint8(0); //値読み取り
-        
-        document.getElementById('valDisplay').innerHTML = v;
-    })
-
-    requestAnimationFrame(valueReception());
 }
